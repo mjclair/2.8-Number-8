3b09459e23c6bb7e100ef5a1f8f8ede499b7a991
Michael Clair <michaelclair@students.abtech.edu>
2025-01-25 11:29:00 -0500

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,12 @@
+
+# addition
+print("Addition: 6 + 2 =", 6 + 2)
+
+#subtraction
+print("Subtraction: 9 - 1 =", 9 - 1)
+
+#multiplication
+print("Multiplication: 4 * 2 =", 4 * 2)
+
+#division
+print("Division: 16 / 2 =", 16 / 2)
